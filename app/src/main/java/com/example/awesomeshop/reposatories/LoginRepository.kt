bea0a26a0db0a85a6ecbb4e14defb348bc1bca6e
almuheetushihab <almuheetu@gmail.com>
2024-10-09 17:44:10 +0600

    refactor some naming convence

--- app/src/main/java/com/example/awesomeshop/reposatories/LoginRepository.kt
+++ app/src/main/java/com/example/awesomeshop/reposatories/LoginRepository.kt
@@ -2,16 +2,15 @@ package com.example.awesomeshop.reposatories
 
 import com.example.awesomeshop.models.login.LoginRequest
 import com.example.awesomeshop.models.login.LoginResponse
-import com.example.awesomeshop.networks.ApiClient
 import com.example.awesomeshop.networks.ApiInterface
 import retrofit2.Response
 import javax.inject.Inject
 
 class LoginRepository @Inject constructor(
-    private val apiInterface: ApiInterface
+    private val loginInterface: ApiInterface
 ) {
 
     suspend fun login(username: String, password: String): Response<LoginResponse> {
-        return apiInterface.login(LoginRequest(username, password))
+        return loginInterface.login(LoginRequest(username, password))
     }
 }
